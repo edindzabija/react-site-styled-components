904542d403b815d6b9be2a2eaaeee0390fddc723
edindzabija <edindzabija@gmail.com>
2020-11-19 00:06:27 +0100

    info height

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const InfoContainer = styled.section`
   color: #fff;
   background: ${({ lightBg }) => (lightBg ? '#fff' : '#000')};
   height: 100vh;
+  min-height: 780px;
   max-height: 960px;
   padding: 0 15px;
 
   @media screen and (max-width: 768px) {
     padding: 100px 24;
   }
-`
+`;
 
 export const InfoWrapper = styled.div`
   display: grid;
@@ -21,7 +22,7 @@ export const InfoWrapper = styled.div`
   margin-right: auto;
   margin-left: auto;
   justify-content: center;
-`
+`;
 
 export const InfoRow = styled.div`
   display: grid;
@@ -34,24 +35,24 @@ export const InfoRow = styled.div`
     grid-template-areas: ${({ imgStart }) =>
       imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
   }
-`
+`;
 
 export const Column1 = styled.div`
   margin-bottom: 25px;
   padding: 0 25px;
   grid-area: col1;
-`
+`;
 
 export const Column2 = styled.div`
   margin-bottom: 25px;
   padding: 0 25px;
   grid-area: col2;
-`
+`;
 export const TextWrapper = styled.div`
   max-width: 540px;
   padding-top: 0;
   /* padding-bottom: 30px; */
-`
+`;
 
 export const TopLine = styled.p`
   color: ${({ lightBg }) => (lightBg ? '#ac6111' : '#ff8200')};
@@ -61,7 +62,7 @@ export const TopLine = styled.p`
   letter-spacing: 1.4px;
   text-transform: uppercase;
   margin-bottom: 16px;
-`
+`;
 
 export const Heading = styled.h2`
   margin-bottom: 24px;
@@ -73,7 +74,7 @@ export const Heading = styled.h2`
   @media screen and (max-width: 720px) {
     font-size: 32px;
   }
-`
+`;
 
 export const Subtitle = styled.p`
   max-width: 440px;
@@ -81,17 +82,17 @@ export const Subtitle = styled.p`
   font-size: 18px;
   line-height: 24px;
   color: ${({ darkText }) => (darkText ? '#010606' : '#fff')};
-`
+`;
 
 export const BtnWrap = styled.div`
   display: flex;
   justify-content: flex-start;
-`
+`;
 
 export const ImgWrap = styled.div`
   max-width: 555px;
   height: 100%;
-`
+`;
 
 export const Img = styled.img.attrs({
   loading: 'lazy',
@@ -99,4 +100,4 @@ export const Img = styled.img.attrs({
   width: 100%;
   margin: 0 0 10px 0;
   padding-right: 0;
-`
+`;
